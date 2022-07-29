a63bedf71a336b44fe5df48e449cc0b783c5aa24
koushik nag shuvo <koushiknagshuvo@gmail.com>
2022-07-29 23:51:17 +0600

    new

--- 1016.cpp
+++ 1016.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
-#include <iomanip>
 using namespace std;
 
 int main()
 {
-    int A, B;
-    cin >> A >> B;
-    double Output = (A * B) / 12.0;
-    cout << fixed << setprecision(3) << Output << endl;
+    int distance;
+    cin >> distance;
+    int time = distance * 2;
+    cout << time << " minutos" << endl;
     return 0;
 }
